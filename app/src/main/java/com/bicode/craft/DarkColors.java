76570cdce13cec1c03295d1b732f37448b086dd4
Hosni <hosnifraj10@gmail.com>
2022-02-27 15:15:04 +0100

    refactor: removed unused blank spaces

--- app/src/main/java/com/bicode/craft/DarkColors.java
+++ app/src/main/java/com/bicode/craft/DarkColors.java
@@ -7,8 +7,6 @@ public class DarkColors {
     public static ArrayList<String> getColors() {
         ArrayList<String> colors = new ArrayList<String>();
 		colors.add(0, "#CCCCCC"); // Description TextView
-		
-		
         return colors;
     }
 	
