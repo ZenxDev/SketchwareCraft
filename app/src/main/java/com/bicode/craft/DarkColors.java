57bfcd3d22bef8e01835cbb69dfa3e44dc33325f
Hosni <hosnifraj10@gmail.com>
2022-02-27 15:00:01 +0100

    feat: added dark color

--- app/src/main/java/com/bicode/craft/DarkColors.java
+++ app/src/main/java/com/bicode/craft/DarkColors.java
@@ -1,9 +1,12 @@
 package com.bicode.craft;
 
+import java.util.ArrayList;
+
 public class DarkColors {
 	
     public static ArrayList<String> getColors() {
-
+        ArrayList colors = new ArrayList<String>;
+		
         return null;
     }
 	
